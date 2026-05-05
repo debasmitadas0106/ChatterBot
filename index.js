61d684ddfd3ffc545030bdf6521f9d7fbd78542b
Debasmita Das <debasmita@vodex.ai>
2026-05-06 02:17:52 +0530

    index.js updated

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log('Jay Shree Ram')
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
@@ -14,7 +13,14 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("Hi the app is working");
 });
+
+app.get("/api-check", (req, res) => {
+  console.log("Jay shree ram");
+  res.send("LogDoor has started well");
+});
+
 app.post("/create-user", createUserController);
+
 app.listen(port, () => {
   console.log(`app is listening at port ${port}`);
 });
